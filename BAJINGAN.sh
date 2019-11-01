5b3402fb2cb5f18595b15cd09a4eab08f8b7502e
Cahyo-Hackers <57224555+Cahyo-Hackers@users.noreply.github.com>
2019-11-01 14:58:04 +0000

    Update BAJINGAN.sh

--- BAJINGAN.sh
+++ BAJINGAN.sh
@@ -20,11 +20,11 @@ echo $red
 cat asw.txt
 echo ""
 echo "\033[34;1m[$]\033[36;1m============================\033[34;1m[$]"
-echo "\033[32;1mAuthor : Mr.B4J1N64N"
-echo "\033[37;1mFeat: Mr.IM81"
+echo "\033[32;1mAuthor : Adi Cy"
+echo "\033[37;1mFeat: Mr.Ac
 echo "\033[35;1mversion Tools: 6"
 echo "\033[35;1mTeam : DARKNESS CYBER TEAM"
-echo "\033[33;1mKontak Me :  083108271123"
+echo "\033[33;1mKontak Me :  087720393195"
 echo "\033[31;1mPlease For Subcribe Youtube:"
 echo "\033[92mDarkness Cyber Team" 
 echo "\033[36;1mIndonesia Security Lite"
